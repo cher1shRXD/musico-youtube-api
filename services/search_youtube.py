db050efdeeab6d57d573407ab08a68414379089f
cher1shRXD <taewoo080401@gmail.com>
2024-11-18 20:27:00 +0900

    search keyword

--- services/search_youtube.py
+++ services/search_youtube.py
@@ -3,6 +3,6 @@ import innertube
 client = innertube.InnerTube("WEB")
 
 def search_youtube(query):
-  data = client.search(query=f'{query} lyrics')
+  data = client.search(query=f'{query} lyrics 가사')
   print(data['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'])
   return data['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']
