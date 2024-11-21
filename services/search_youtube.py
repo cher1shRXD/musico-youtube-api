16de5264b2f5ea5d4558d9d76dbcbfd57be6efb8
cher1shRXD <taewoo080401@gmail.com>
2024-11-21 17:08:21 +0900

    refactor: return videoIds directly

--- services/search_youtube.py
+++ services/search_youtube.py
@@ -6,4 +6,5 @@ def search_youtube(query):
   data = client.search(query=f'{query} lyrics 가사')
   filterdData = data['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']
   onlyVideo = [obj for obj in filterdData if obj.get('videoRenderer') is not None]
-  return onlyVideo
+  videoIds = [onlyVideo[0]['videoRenderer']['videoId'], onlyVideo[1]['videoRenderer']['videoId'], onlyVideo[2]['videoRenderer']['videoId']]
+  return videoIds
